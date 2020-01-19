f6452e97e39a5011ea7cbc2b63975f9f2886dd29
Joelmukono <jlmukono@gamil.com>
2020-01-19 13:39:16 +0300

    add interface methods to user dao file

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,5 +1,18 @@
 package dao;
+import models.User;
+import java.util.List;
 
 public interface UserDao {
+
+    void add(User user);
+
+    List<User> getAll();
+
+    User findById(int id);
+
+
+
+
+
 
 }
