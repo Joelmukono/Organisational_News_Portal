e45798d56d272e3fffcf7c326003fefb31572424
Joelmukono <jlmukono@gamil.com>
2020-01-19 13:16:29 +0300

    add user model and userdao files

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,8 +1,17 @@
 package models;
 
 public class User {
+
     private int id;
     private String userName;
     private String department;
     private String placement;
+
+    public User(int id, String userName,String department, String placement){
+        this.id = id;
+        this.userName = userName;
+        this.department = department;
+        this.placement = department;
+
+    }
 }
